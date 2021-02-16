6364b1356a50766d7a030450be857e416410afb0
AlexanderSlaa <alexanderslaa2000@gmail.com>
2021-02-17 00:29:48 +0100

    Change POST.java to working example

---
+++ tests/Service.java
@@ -0,0 +1,27 @@
+import com.openwebserver.core.Objects.Request;
+import com.openwebserver.core.Objects.Response;
+import com.openwebserver.services.Annotations.Route;
+
+
+public class Service extends com.openwebserver.services.Objects.Service {
+
+    public Service(String path) {
+        super(path);
+    }
+
+    @Route(path = "/", method = Method.POST)
+    public Response post(Request request) {
+        return Response.simple(request.POST());
+    }
+
+    @Route(path = "/", method = Method.GET)
+    public Response get(Request request) {
+        return Response.simple(request.POST());
+    }
+
+    @Route(path = "/{id}", method = Method.GET)
+    public Response id(Request request) {
+        return Response.simple(request.GET());
+    }
+
+}
