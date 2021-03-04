7e395bb82c4af0d7fb7428efb011e16d1b514d10
AlexanderSlaa <55486167+AlexanderSlaa@users.noreply.github.com>
2021-03-04 20:13:22 +0100

    Cleanup

--- tests/Main.java
+++ tests/Main.java
@@ -13,25 +13,11 @@ import com.openwebserver.core.WebServer;
 public class Main {
 
     public static void main(String[] args) {
-
-        PolicyManager.Register(new Policy("all").AllowAnyHeader().AllowAnyOrgin().AllowAnyMethods());
-
         new WebServer().addDomain(
                 new Domain()
-                    .addHandler(new RequestHandler(new Route("#", Route.Method.GET), request -> Response.simple(Code.Ok)))
+                    .addHandler(new Service("/"))
         ).start();
         Router.print();
-
-//        JsonWebToken token = new JsonWebToken();
-//        token.PAYLOAD.put("sub", "1234567890");
-//        token.PAYLOAD.put("name", "John Doe");
-//        token.PAYLOAD.put("iat", 1516239022);
-//
-//        String jwt = token.sign("your-256-bit-secret");
-//
-//        System.out.println(new JsonWebToken(jwt).verify("you-256-bit-secret"));
-
-
     }
 
 }
