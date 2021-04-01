13703ef60111290ea73b9e4f72bf9d0e1a7d631c
AlexanderSlaa <55486167+AlexanderSlaa@users.noreply.github.com>
2021-04-01 11:45:15 +0200

    Reformat Router.java
    Added VERBOSE mode in Route for logging incomming connections

--- src/com/openwebserver/core/Routing/Router.java
+++ src/com/openwebserver/core/Routing/Router.java
@@ -1,7 +1,6 @@
 package com.openwebserver.core.Routing;
 
 import ByteReader.ByteReader.ByteReaderException.PrematureStreamException;
-
 import com.openwebserver.core.Connection.Connection;
 import com.openwebserver.core.Content.Code;
 import com.openwebserver.core.Domain;
@@ -17,35 +16,37 @@ import java.util.concurrent.atomic.AtomicReference;
 public class Router {
 
     private static final Router router = new Router();
+    public static boolean VERBOSE = false;
 
     private final TreeArrayList<Domain, Routes> routes = new TreeArrayList<>();
 
-    private Router(){}
+    private Router() {
+    }
 
-    public static void register(Domain domain, RequestHandler handler){
+    public static void register(Domain domain, RequestHandler handler) {
         getInstance().routes.populate(domain);
         Routes routes = null;
         for (Routes routes1 : getInstance().routes.branch(domain)) {
-            if(routes1.getPath().equals("#")){
+            if (routes1.getPath().equals("#")) {
                 System.out.println("Routes wildcard found");
                 break;
             }
-            if(routes1.getPath().equals(handler.getPath())){
+            if (routes1.getPath().equals(handler.getPath())) {
                 routes1.add(handler);
                 routes = routes1;
             }
         }
-        if(routes == null){
-            getInstance().routes.addOn(domain,new Routes(handler.getPath(), domain).add(handler));
+        if (routes == null) {
+            getInstance().routes.addOn(domain, new Routes(handler.getPath(), domain).add(handler));
         }
     }
 
-    public static ArrayList<Routes> getRoutes(Domain domain){
+    public static ArrayList<Routes> getRoutes(Domain domain) {
         return getInstance().routes.get(domain);
     }
 
-    public static void handle(Connection connection){
-        connection.handle((self, args) ->{
+    public static void handle(Connection connection) {
+        connection.handle((self, args) -> {
             try {
                 Request request = Request.deserialize(self);
                 self.write(Router.find(request, self).handle(request));
@@ -55,18 +56,23 @@ public class Router {
                 self.write(e.respond());
             } catch (Throwable throwable) {
                 self.write(new WebException(throwable).respond(true));
+            } finally {
+                if (VERBOSE) {
+                    System.out.println(connection.getConnectionString());
+                }
             }
         });
     }
 
+
     private static Routes find(Request request, Connection connection) throws RoutingException.NotFoundException {
         AtomicReference<Routes> requestHandler = new AtomicReference<>(null);
         router.routes.Search(domain -> domain.getAlias().equals(request.getAlias()) && domain.getPort() == connection.getLocalPort(), handlers -> handlers.forEach(routes -> {
-                if(routes.matches(request)){
-                    requestHandler.set(routes);
-                }
-            }));
-        if(requestHandler.get() == null){
+            if (routes.matches(request)) {
+                requestHandler.set(routes);
+            }
+        }));
+        if (requestHandler.get() == null) {
             throw new RoutingException.NotFoundException(request.getPath(true));
         }
         return requestHandler.get();
@@ -74,14 +80,14 @@ public class Router {
 
     public static Domain getDomain(String host) {
         for (Domain domain : router.routes.keySet()) {
-            if(domain.getAlias().equals(host)){
+            if (domain.getAlias().equals(host)) {
                 return domain;
             }
         }
         return null;
     }
 
-    public static Router getInstance(){
+    public static Router getInstance() {
         return router;
     }
 
@@ -111,10 +117,10 @@ public class Router {
             super(code, message);
         }
 
-        public static class NotFoundException extends RoutingException{
+        public static class NotFoundException extends RoutingException {
 
             public NotFoundException(String path) {
-                super(Code.Not_Found,"Can't find route for '"+path+"'");
+                super(Code.Not_Found, "Can't find route for '" + path + "'");
             }
         }
     }
