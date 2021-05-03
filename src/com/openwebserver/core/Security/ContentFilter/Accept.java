1d5080206aef1b3cf57add28c0fb769ae98a8d7c
AlexanderSlaa <55486167+AlexanderSlaa@users.noreply.github.com>
2021-05-03 09:43:07 +0200

    Update Accept.java
    Added Retention and Target Class Annotations

--- src/com/openwebserver/core/Security/ContentFilter/Accept.java
+++ src/com/openwebserver/core/Security/ContentFilter/Accept.java
@@ -1,5 +1,12 @@
 package com.openwebserver.core.Security.ContentFilter;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Accept {
     String[] value();
 }
